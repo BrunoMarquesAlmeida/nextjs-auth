c176387ed016f4c9211aa970b2518edf03529539
BrunoMarquesAlmeida <brunom.almeida@hotmail.com>
2023-02-06 16:32:01 +0000

    set up conditional rendering + redirects for auth

--- pages/auth.js
+++ pages/auth.js
@@ -1,6 +1,26 @@
-import AuthForm from '../components/auth/auth-form';
+import { getSession } from "next-auth/client";
+import { useEffect, useState } from "react";
+import Router, { useRouter } from "next/router";
+
+import AuthForm from "../components/auth/auth-form";
 
 function AuthPage() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        Router.replace("/");
+      } else {
+        setIsLoading(false);
+      }
+    });
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return <AuthForm />;
 }
 
