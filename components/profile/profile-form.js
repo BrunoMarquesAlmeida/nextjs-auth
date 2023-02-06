fb48e8a68210ae3773436e941fa4494cb46330f2
BrunoMarquesAlmeida <brunom.almeida@hotmail.com>
2023-02-06 17:18:43 +0000

    added change user password functionality

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,15 +1,32 @@
-import classes from './profile-form.module.css';
+import { useRef } from "react";
 
-function ProfileForm() {
+import classes from "./profile-form.module.css";
+
+function ProfileForm(props) {
+  const oldPasswordRef = useRef();
+  const newPasswordRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredOldPasword = oldPasswordRef.current.value;
+    const enterdNewPassword = newPasswordRef.current.value;
+
+    props.onChangePassword({
+      oldPassword: enteredOldPasword,
+      newPassword: enterdNewPassword,
+    });
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={newPasswordRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <label htmlFor="old-password">Old Password</label>
+        <input type="password" id="old-password" ref={oldPasswordRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
